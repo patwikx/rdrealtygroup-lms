12493e2df8055de52684b42758a3a81f51007db8
Patrick <plmiranda@rdretailgroup.com.ph>
2025-07-29 08:37:32 +0800

    Fix: spacing issue in approver page

--- app/dashboard/approver/page.tsx
+++ app/dashboard/approver/page.tsx
@@ -25,7 +25,7 @@ export default async function SettingsPageMain() {
         <SiteHeader />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+  <div className="gap-4 space-y-6 px-4 lg:px-6">
               <ApproverPageWrapper />
                  {/* Stats */}
             </div>
